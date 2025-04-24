476b5cfd3f23bf94b64fadde8eb6e8b6e91a6205
climatechange-tech <jagabantxo@gmail.com>
2025-04-24 09:00:33 +0200

    Improve sub-package import structure
    - Replace direct imports with `__all__` definition:
      from . import auxiliary_functions
      from . import ba_mean
      from . import ba_mean_and_var
      ...
      ↓
      __all__ = [
          auxiliary_functions,
          ba_mean,
          ba_mean_and_var,
          ...
      ]
    
    This change aligns the `supplementary_tools` sub-package with the parent package´s
    import philosophy, providing better control over exported symbols
    when using `from climalab.supplementary_tools import *`. The public API remains
    unchanged, but is now more explicitly defined.

--- supplementary_tools/__init__.py
+++ supplementary_tools/__init__.py
@@ -3,14 +3,16 @@
 
 # climalab/supplementary_tools/__init__.py
 
-from . import __init__
-from . import auxiliary_functions
-from . import ba_mean
-from . import ba_mean_and_var
-from . import ba_nonparametric_qm
-from . import ba_parametric_qm
-from . import basic_mapplot
-from . import basic_plot
-from . import comparison_lineplot
-from . import eval_original
-from . import temperature_map
+# Define what should be available when using 'from climalab.supplementary_tools import *'
+__all__ = [
+    'auxiliary_functions',
+    'ba_mean',
+    'ba_mean_and_var',
+    'ba_nonparametric_qm',
+    'ba_parametric_qm',
+    'basic_mapplot',
+    'basic_plot',
+    'comparison_lineplot',
+    'eval_original',
+    'temperature_map'
+]
