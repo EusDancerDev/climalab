841eebbe4366fa4fbaf374f418fcfeac722e8e4c
climatechange-tech <jagabantxo@gmail.com>
2025-04-24 09:21:16 +0200

    Improve third-level sub-package import structure in 'data_analysis_projects_sample'
    - Replace direct imports with `__all__` definition:
      from . import cds_tools
      from . import download_cordex
      from . import download_eobs
      from . import download_era5
      from . import download_era5_land
      ↓
      __all__ = [
          `cds_tools`,
          `download_cordex`,
          `download_eobs`,
          `download_era5`,
          `download_era5_land`
      ]
    
    This change aligns the `data_analysis_projects_sample.src.data` sub-package with the parent package´s
    import philosophy, providing better control over exported symbols
    when using `from climalab.data_analysis_projects_sample.src.data import *`. The public API remains
    unchanged, but is now more explicitly defined.

--- data_analysis_projects_sample/src/data/__init__.py
+++ data_analysis_projects_sample/src/data/__init__.py
@@ -3,8 +3,11 @@
 
 # climalab/data_analysis_projects_sample/src/data/__init__.py
 
-from . import cds_tools
-from . import download_cordex
-from . import download_eobs
-from . import download_era5
-from . import download_era5_land
+# Define what should be available when using 'from climalab.data_analysis_projects_sample.src.data import *'
+__all__ = [
+    'cds_tools',
+    'download_cordex',
+    'download_eobs',
+    'download_era5',
+    'download_era5_land'
+]
